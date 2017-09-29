9320809f74453f87dfdd6e01d231aa0e92dbf652
shiyu <shiyu@cs.ucsb.edu>
2017-09-28 20:38:14 -0700

    generalize diff.sh

--- diff.sh
+++ diff.sh
@@ -3,6 +3,6 @@ for i in 0 1 2 3 4 5 6 7 24 25 26 27 28 29
 do
   (( j++ ))
   echo $((i+3)) $j
-  diff <(cat small_sample_1word_combined_features.txt | cut -d ' ' -f $((i+3)) | cut -d ':' -f 2) <(cat small_sample_1word_subfeatures.txt | cut -d ' ' -f $j | cut -d ':' -f 2)
+  diff <(cat $1 | cut -d ' ' -f $((i+3)) | cut -d ':' -f 2) <(cat $2 | cut -d ' ' -f $j | cut -d ':' -f 2)
 done
 
