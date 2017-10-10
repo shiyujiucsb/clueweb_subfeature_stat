901c2eb4bcd1374236d32b2e32b5f4c731f2d63f
shiyu <shiyu@cs.ucsb.edu>
2017-10-09 18:09:40 -0700

    no many processes crash

--- run_map.sh
+++ run_map.sh
@@ -7,7 +7,7 @@ do
   for nLeaves in 10 20 30 40 50
   do
     echo $nTrees $nLeaves $filename
-    java -Xmx32000m -jar ./RankLib-2.1-patched.jar -shrinkage 0.1 -train $filename -ranker 6 -kcv 5 -kcvmd models/ -kcvmn ca -tree $nTrees -leaf $nLeaves -metric2t MAP -metric2T MAP > 'MAP_rankres_'$1'_'$nTrees'_'$nLeaves'.txt' &
+    time java -Xmx32000m -jar ./RankLib-2.1-patched.jar -shrinkage 0.1 -train $filename -ranker 6 -kcv 5 -kcvmd models/ -kcvmn ca -tree $nTrees -leaf $nLeaves -metric2t MAP -metric2T MAP > 'MAP_rankres_'$1'_'$nTrees'_'$nLeaves'.txt' 
   done
 done
 
